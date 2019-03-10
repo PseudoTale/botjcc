0bb4f68f8f795aca1ee4821320d57247b3a68f4d
PseudoTale <48414323+PseudoTale@users.noreply.github.com>
2019-03-10 18:56:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -141,12 +141,15 @@ bot.on('message', async message => {
 		}
 	
 			if(message.content === '.PseudoTale') {
-			console.log(`Commande .gif par ${message.author.tag}`)
+			console.log(`Commande .PseudoTale par ${message.author.tag}`)
 			return message.channel.send("https://www.youtube.com/channel/UCUspavUwM6UFeFRxmwVHiwA")
 			}
 	
 	})
-
+			if(message.content === '.Pewds') {
+			console.log(`Commande .Pewds par ${message.author.tag}`)
+			return message.channel.send("abbonez vous a Pewdiepie ! https://www.youtube.com/user/PewDiePie")
+			}
 
 
 
