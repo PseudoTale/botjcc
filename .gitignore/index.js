5f8aa797c4526b1ac41b20fcfbe95ebdbb80b32d
Azerptiop <48407596+Azerptiop@users.noreply.github.com>
2019-03-11 06:37:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -158,6 +158,30 @@ bot.on('message', async message => {
 				message.channel.bulkDelete(parseInt(count) + 1)
 
 			}
+
+	if (args[0].toLowerCase() === ".mute") {
+        	if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        	let member = message.mentions.members.first()
+        	if (!member) return message.channel.send("Membre introuvable")
+        	if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+        	if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
+        	let muterole = message.guild.roles.find(role => role.name === 'Muted')
+        	if (muterole) {
+           	 member.addRole(muterole)
+           	 message.channel.send(member + ' a été mute :white_check_mark:')
+        }
+        else {
+            message.guild.createRole({name: 'Muted', permissions: 0}).then((role) => {
+                message.guild.channels.filter(channel => channel.type === 'text').forEach(channel => {
+                    channel.overwritePermissions(role, {
+                        SEND_MESSAGES: false
+                    })
+                })
+                member.addRole(role)
+                message.channel.send(member + ' a été mute :white_check_mark:')
+            })
+        }
+    }
 	
 	})
 
